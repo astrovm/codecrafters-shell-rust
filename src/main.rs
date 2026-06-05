168b64dde004b8ef9b7a0462c1fe572be19cd7b1
astrovm <~@4st.li>
2026-06-05 11:50:12 -0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,11 @@ fn main() {
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
 
+        // If the command is the builtin "exit", break the loop
+        if input.trim() == "exit" {
+            break;
+        }
+
         // Report that the entered command is unknown
         println!("{}: command not found", input.trim())
     }
