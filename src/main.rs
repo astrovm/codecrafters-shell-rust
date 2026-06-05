530d0a6e2647d5d976d4b49ac2263459b5aabc6e
astrovm <~@4st.li>
2026-06-05 14:51:34 -0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::io::{self, Write};
+use std::os::unix::fs::PermissionsExt;
+use std::path::PathBuf;
 
 fn main() {
     loop {
@@ -31,7 +33,27 @@ fn type_command(input: &str) -> String {
     // Check whether the requested command is a shell built-in
     if builtin_commands.contains(&input) {
         format!("{} is a shell builtin", input)
+    } else if let Some(full_path) = find_executable_in_path(input) {
+        format!("{} is {}", input, full_path.display())
     } else {
         format!("{}: not found", input)
     }
+}
+
+fn find_executable_in_path(command: &str) -> Option<PathBuf> {
+    let path = std::env::var("PATH").unwrap_or_default();
+
+    // Search each directory in PATH for the requested command
+    for dir in std::env::split_paths(&path) {
+        let full_path = dir.join(command);
+
+        // Return the first regular file with an executable permission bit
+        if let Ok(metadata) = full_path.metadata()
+            && metadata.is_file()
+            && metadata.permissions().mode() & 0o111 != 0
+        {
+            return Some(full_path);
+        }
+    }
+    None
 }
